c8a4bb1c99a4788950ce30b0d07c6fc1ddeafa2f
Stacey Dale <sdale@ara.com>
2024-07-26 11:32:04 -0400

    Add euclid activitiy

---
+++ euclid_pyramid_1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Welcome to the Euclidean Triangle (Pyramid) challenge.");
+    println!("The challenge goes as follows:");
+    println!("\nGiven a pyramid of the structure\n");
+    println!("\t\t\t  [3]\n\t\t\t[10,20]\n\t\t       [55,23,7]\n\t\t     [24,25,26,27]\n\t\t   [42,46,40,39,22]\n");
+    println!("\t1. Read the file in as a command line argument.");
+    println!("\t2. Return (print) the greates sum from top to bottom.");
+}
